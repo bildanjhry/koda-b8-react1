0407ef2c31816e38650810939b5114df3e331d9a
bildan <bildanjauhary@gmail.com>
2026-06-11 12:53:20 +0700

    feat: add feature if state more than 10 then cannot go up more

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   const handleDecrement = () => count > 0 && setCount(count-1)
-  const handleIncrement = () => setCount(count+1)
+  const handleIncrement = () => count < 10 && setCount(count+1)
 
   return (
     <div className='w-full h-[100vh] flex justify-center items-center'>
